93cd3542e9b4d92f4735b9cf50b4d55fb0011b12
Htun Thein Win <phochit77@gmail.com>
2025-03-02 12:53:29 +0630

    Solid,Regex

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpression
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            #region Regular Expression
+
+            // Basic Match
+            // . 
+            // \d => 0 to 9
+            // \D 
+            // \w => a to z, A to Z, 0 to 9, _
+            // \W
+            // \s => spaces,tabs,newlines
+            // \S
+            // dupe, \k
+
+
+            // Anchor
+            // ^ => Begin
+            // $ => End
+
+            // Quantifiers
+            // * => 0 or More
+            // + => 1 or More
+            // ? => Optional
+            // {x} => 
+
+            // user DOB (YYYY-MM-DD)
+            // 2000-01-01
+
+            // ^\d{4}-\d{2}-\d{2}$
+
+            // user IP address
+            // 192.168.1.120
+
+            // ^(\d{1,3}\.){3}\d{1,3}$
+
+            // User Email
+            // abcd@gmail.info
+
+            // ^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$
+            // ^\w+@\w\.\w{2,}$
+
+            // User URL
+            // http or https or ftp
+
+            // User Favourite Color Code (#RRGGBB)
+            // #FF0000
+            // #F00
+
+            // ^#?([a-fA-F0-9]{6} | [a-fA-F0-9]{3})$
+            #endregion
+
+            string pattern = @"\d+";
+            string patDOB = @"^\d{4}-\d{2}-\d{2}$";
+
+            Console.Write("Enter DOB(YYYY-MM-DD) : ");
+            string check=Console.ReadLine();
+
+            Regex reg = new Regex(patDOB);
+
+            if(reg.IsMatch(check))
+            {
+                Console.WriteLine("Valid Date of Birth");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Date of Birth");
+            }
+
+            var sigleResult=reg.Match(check);
+
+            // Replace, Split
+
+            //MatchCollection matches = reg.Matches(check);
+
+            //foreach (Match match in matches)
+            //{
+            //    Console.WriteLine(match.Value);
+            //}
+
+            // Strong Password
+            // Special Character 1
+            // Uppercase 1
+            // Lowercase 1
+            // Number 1
+            // Character lenght 8s
+
+            // public ip
+
+        }
+    }
+}
---
+++ SOLID/Program.cs
@@ -0,0 +1,233 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// SOLID Principle
+
+using System;
+
+namespace Solid
+{
+	#region SRP
+	class Freezer
+	{
+		void PutInTheFrig(string things)
+		{
+			if (Things.CheckThings("water"))
+			{
+				// OK
+			}
+			else
+			{
+				// Not Ok
+			}
+		}
+	}
+
+	class Things
+	{
+		public static bool CheckThings(string thing)
+		{
+			return true;
+		}
+	}
+	#endregion
+
+	#region Open_Closed Priciple(OCP)
+
+	// it works, dont touch it
+
+	// SDLC (SAD)
+	// SRS
+
+	//class SellPrice
+	//{
+	//       public decimal Price { get; set; }
+	//   }
+
+	//class PurchasePrice
+	//{
+	//       public decimal Price { get; set; }
+	//   }
+
+	//class Cost
+	//{
+	//	public decimal Price { get; set; }
+	//}
+
+	abstract class PriceType
+	{
+		public abstract decimal Price();
+	}
+
+	class SellPrice : PriceType
+	{
+		public decimal sPrice { get; set; }
+		public override decimal Price()
+		{
+			return sPrice;
+		}
+	}
+
+	class PurchasePrice : PriceType
+	{
+		public decimal pPrice { get; set; }
+		public override decimal Price()
+		{
+			return pPrice;
+		}
+	}
+
+	class GrandTotalCalculator
+	{
+		public decimal GrandTotal(PriceType[] items)
+		{
+			decimal tot = 0;
+			foreach (var item in items)
+			{
+				tot += item.Price();
+			}
+			return tot;
+		}
+	}
+
+	#endregion
+
+	#region Liskov Substitution Priciple(LSP)
+
+	// Birds can fly
+	// Penguin
+
+	abstract class Discout
+	{
+		public abstract decimal ApplyDiscount(decimal amt);
+
+	}
+
+	class PercentageDiscount : Discout
+	{
+		public decimal percentage { get; set; }
+
+		public override decimal ApplyDiscount(decimal amt)
+		{
+			return amt - (amt * percentage / 100);
+		}
+	}
+
+	class FixedAmountDiscount : Discout
+	{
+		public decimal FixedAmount { get; set; }
+
+		public override decimal ApplyDiscount(decimal amt)
+		{
+			return amt - FixedAmount;
+		}
+	}
+
+	#endregion
+
+	#region Interface Segregation Priciple(ISP)
+
+	public interface ISample
+	{
+		void Save();
+		void Show();
+
+		void Update();
+
+		void Delete();
+
+		void ShowAll();
+	}
+
+	class CallInterface : ISample
+	{
+		public void Delete()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Save()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Show()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void ShowAll()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update()
+		{
+			throw new NotImplementedException();
+		}
+	}
+
+	#endregion
+
+	#region Dependency Inversion Priciple(DIP)
+	// Constructor
+
+	public interface IOrderValidator
+	{
+		bool ValidateOrder(Order order);
+	}
+
+	public interface IPaymentProcessor
+	{
+		void ProcessPayment(decimal amount);
+	}
+
+	public interface IDiscount
+	{
+		decimal ApplyDiscount(Order order);
+	}
+
+	public class Order
+	{
+		public Item[] Items { get; set; }
+
+	}
+
+	public class Item
+	{
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		public decimal Price {  get; set; }
+	}
+
+	public class OrderProcessor
+	{
+		private readonly IOrderValidator orderValidator;
+		private readonly IPaymentProcessor paymentProcessor;
+		private readonly IDiscount discount;
+
+		public OrderProcessor(IOrderValidator ov,IPaymentProcessor pp,IDiscount ds)
+		{
+			orderValidator = ov;
+			paymentProcessor = pp;
+			discount = ds;
+		}
+
+		public void OrderProcess(Order order)
+		{
+			if(orderValidator.ValidateOrder(order))
+			{
+				var dis=discount.ApplyDiscount(order);
+				var total = order.Items.Sum(i => i.Price) - dis;
+				paymentProcessor.ProcessPayment(total);
+			}
+		}
+	}
+	#endregion
+
+	// Design Pattern(GOF)
+	// Creational
+	// Structural
+	// Behavioral
+}
